a9ae914095fbe4817a25ecf206f5a8acaa5eba95
Roberto Izquierdo <RobertoIA1987@gmail.com>
2014-03-01 14:03:23 +0100

    [finishes #66709476] Exporta documentos a HTML

---
+++ src/test/java/es/ubu/inf/tfg/doc/Documento.java
@@ -0,0 +1,8 @@
+package es.ubu.inf.tfg.doc;
+
+import es.ubu.inf.tfg.asu.AhoSethiUllman;
+
+public interface Documento {
+	public void añadirProblema(AhoSethiUllman asu);
+	public String toString();
+}
