8b4ed88b1e445d4b009703f6c1599f0fb3b9ca9c
Roberto Izquierdo <RobertoIA1987@gmail.com>
2014-03-18 13:51:57 +0100

    Documenta Automata.

--- src/main/java/es/ubu/inf/tfg/regex/thompson/datos/Automata.java
+++ src/main/java/es/ubu/inf/tfg/regex/thompson/datos/Automata.java
@@ -5,12 +5,30 @@ import java.util.TreeSet;
 
 import es.ubu.inf.tfg.regex.datos.ExpresionRegular;
 
+/**
+ * Automata implementa el modelo lógico de un automata finito no determinista,
+ * compuesto por una serie de nodos y de transiciones entre dichos nodos.
+ * Permite obtener los nodos obtenidos a partir de uno dado utilizando un tipo
+ * de transición determinado, y consumiendo o no un caracter dado.
+ * 
+ * @author Roberto Izquierdo Amo
+ * 
+ */
 public class Automata {
 
 	private Nodo nodoInicial;
 	private Nodo nodoFinal;
 
+	/**
+	 * Constructor. Define un automata finito no determinista a partir de un
+	 * árbol de expresion regular dado, de manera recursiva.
+	 * 
+	 * @param expresion
+	 *            Árbol de expresion regular a partir del cual generar el
+	 *            autómata.
+	 */
 	public Automata(ExpresionRegular expresion) {
+		// TODO completamente provisional
 		Nodo nodo0 = new Nodo(0, false);
 		Nodo nodo1 = new Nodo(1, false);
 		Nodo nodo2 = new Nodo(2, false);
@@ -50,14 +68,35 @@ public class Automata {
 		this.nodoFinal = nodo10;
 	}
 
+	/**
+	 * Nodo de entrada del autómata.
+	 * 
+	 * @return Nodo inicial.
+	 */
 	public Nodo nodoInicial() {
 		return this.nodoInicial;
 	}
 
+	/**
+	 * Nodo final del autómata.
+	 * 
+	 * @return Nodo final.
+	 */
 	public Nodo nodoFinal() {
 		return this.nodoFinal;
 	}
 
+	/**
+	 * Obtiene el conjunto de nodos al que se llega a partir de un nodo inicial
+	 * y tras consumir un símbolo determinado. Solo se cuentan las transiciones
+	 * vacías efectuadas tras consumir la entrada.
+	 * 
+	 * @param inicio
+	 *            Nodo de inicio.
+	 * @param simbolo
+	 *            Símbolo de entrada.
+	 * @return Conjunto de nodos de llegada.
+	 */
 	public Set<Nodo> transicion(Nodo inicio, char simbolo) {
 		// Nodos a los que llegamos desde el inicio sin consumir
 		Set<Nodo> iniciales = transicionVacia(inicio);
@@ -81,6 +120,14 @@ public class Automata {
 		return transicionConsumiendo;
 	}
 
+	/**
+	 * Obtiene el conjunto de nodos al que se llega a partir de un nodo inicial
+	 * y sin consumir ningún símbolo.
+	 * 
+	 * @param inicio
+	 *            Nodo de inicio.
+	 * @return Conjunto de nodos de llegada.
+	 */
 	public Set<Nodo> transicionVacia(Nodo inicio) {
 		// Nodos a los que llegamos sin consumir entrada
 		Set<Nodo> actuales = new TreeSet<>();
