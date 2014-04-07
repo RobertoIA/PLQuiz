ef6608f171d6610309a473053a88a1ca5fb564c6
Roberto Izquierdo <RobertoIA1987@gmail.com>
2014-04-07 22:12:05 +0200

    Simplifica el funcionamiento de Generador.

--- src/main/java/es/ubu/inf/tfg/regex/datos/Generador.java
+++ src/main/java/es/ubu/inf/tfg/regex/datos/Generador.java
@@ -6,14 +6,6 @@ import java.util.EnumSet;
 import java.util.List;
 import java.util.Random;
 
-/**
- * Generador implementa una clase encargada de generar árboles de expresión
- * regular de un tamaño y características dadas, con generación de árboles
- * basada en el algoritmo GROW.
- * 
- * @author Roberto Izquierdo Amo.
- * 
- */
 public class Generador {
 
 	private static final Random random = new Random(new Date().getTime());
@@ -22,17 +14,6 @@ public class Generador {
 	private List<Character> simbolosRepetidos;
 	private int posicion;
 
-	/**
-	 * Operador implementa un tipo enumerado que contiene los tipos de
-	 * operadores que utilizará el árbol, los conjuntos de operadores que se
-	 * utilizan en cada circunstancia, y una serie de operaciones relacionadas.
-	 * <p>
-	 * Se utiliza también para almacenar el estado del árbol en un momento
-	 * determinado.
-	 * 
-	 * @author Roberto Izquierdo Amo
-	 * 
-	 */
 	private static enum Operador {
 		SIMBOLO, VACIO, CIERRE, CONCAT, UNION;
 
@@ -44,124 +25,28 @@ public class Generador {
 				SIMBOLO, VACIO);
 		private static final EnumSet<Operador> FINAL_PARCIAL = EnumSet
 				.of(SIMBOLO);
-
-		/**
-		 * Devuelve un operador aleatorio de entre un conjunto de operadores.
-		 * 
-		 * @param operadores
-		 *            Conjunto de operadores entre los que elegir.
-		 * @return Operador cualquiera del conjunto.
-		 */
-		private static Operador random(EnumSet<Operador> operadores) {
-			int index = random.nextInt(operadores.size());
-			return (Operador) operadores.toArray()[index];
-		}
 	}
 
-	/**
-	 * Inicializa los valores del generador para empezar a trabajar en un nuevo
-	 * árbol.
-	 * 
-	 * @param nSimbolos
-	 *            Número de símbolos a generar.
-	 * @param usaVacio
-	 *            <code>true</code> si la expresión contendrá nodos vacíos,
-	 *            <code>false</code> de lo contrario.
-	 */
-	public Generador(int nSimbolos, boolean usaVacio) {
-		simbolos = new ArrayList<>();
+	public ExpresionRegular arbol(int profundidad, int nSimbolos,
+			boolean usaVacio) {
+		simbolosRepetidos = new ArrayList<>();
 		for (int i = 0; i < nSimbolos; i++)
-			simbolos.add((char) ('a' + i));
+			simbolosRepetidos.add((char) ('a' + i));
 		if (usaVacio)
-			simbolos.add('E');
-		simbolosRepetidos = new ArrayList<>(simbolos);
+			simbolosRepetidos.add('E');
+
+		simbolos = new ArrayList<>(simbolosRepetidos);
 
 		posicion = 0;
+
+		return subArbol(profundidad, null);
 	}
 
-	/**
-	 * Comprueba si el árbol contendrá nodos vacíos.
-	 * 
-	 * @return<code>true</code> si la expresión contendrá nodos vacíos,
-	 *                          <code>false</code> de lo contrario.
-	 */
-	public boolean usaVacio() {
+	private boolean usaVacio() {
 		return simbolosRepetidos.contains('E');
 	}
 
-	/**
-	 * Devuelve la cantidad de símbolos distintos que debe contener el árbol.
-	 * 
-	 * @return Cantidad de símbolos en el árbol.
-	 */
-	public int simbolos() {
-		return simbolosRepetidos.size();
-	}
-
-	/**
-	 * Genera un símbolo cualquiera de aquellos que puede incluir el árbol.
-	 * Puede contener el símbolo vacío. Prioritiza símbolos que aún no hayan
-	 * aparecido.
-	 * 
-	 * @return Un símbolo cualquiera o el símbolo vacío.
-	 */
-	private char simbolo() {
-		int index;
-
-		if (simbolos.size() > 0) {
-			index = random.nextInt(simbolos.size());
-			char simbolo = simbolos.get(index);
-			simbolos.remove(index);
-			return simbolo;
-		} else {
-			index = random.nextInt(simbolosRepetidos.size());
-			return simbolosRepetidos.get(index);
-		}
-	}
-
-	/**
-	 * Genera un símbolo cualquiera de aquellos que puede incluir el árbol,
-	 * excluyendo el símbolo vacío. Prioritiza símbolos que aún no hayan
-	 * aparecido.
-	 * 
-	 * @return Un símbolo cualquiera.
-	 */
-	private char simboloNoVacio() {
-		int index;
-
-		if (simbolosRepetidos.contains('E')) {
-			List<Character> simbolosParcial;
-			if (simbolos.size() > 1) {
-				simbolosParcial = new ArrayList<>(simbolos);
-				if (simbolosParcial.contains('E'))
-					simbolosParcial.remove(simbolosParcial.indexOf('E'));
-				index = random.nextInt(simbolosParcial.size());
-				char simbolo = simbolosParcial.get(index);
-				simbolos.remove(index);
-				return simbolo;
-			} else {
-				simbolosParcial = new ArrayList<>(simbolosRepetidos);
-				simbolosParcial.remove(simbolosParcial.indexOf('E'));
-				index = random.nextInt(simbolosParcial.size());
-				return simbolosParcial.get(index);
-			}
-		} else
-			return simbolo();
-	}
-
-	/**
-	 * Genera un sub-árbol de expresion regular con la profundidad dada y
-	 * utilizando un conjunto de operadores concreto. Se llama a si mismo de
-	 * manera recursiva para completar la construcción.
-	 * 
-	 * @param profundidad
-	 *            Profundidad del árbol pedido.
-	 * @param operadores
-	 *            Operadores a utilizar. Cuando se llama desde el exterior de la
-	 *            clase, este argumento será <code>null</code>.
-	 * @return Expresión regular generada.
-	 */
-	public ExpresionRegular subArbol(int profundidad,
+	private ExpresionRegular subArbol(int profundidad,
 			EnumSet<Operador> operadores) {
 
 		ExpresionRegular hijoIzquierdo;
@@ -183,7 +68,7 @@ public class Generador {
 		}
 
 		// Nodo operador.
-		switch (Operador.random(operadores)) {
+		switch (operador(operadores)) {
 		case VACIO: // Vacío solo actua como marcador.
 		case SIMBOLO:
 			char simbolo;
@@ -209,5 +94,47 @@ public class Generador {
 		default:
 			return null;
 		}
+	}
+
+	private Operador operador(EnumSet<Operador> operadores) {
+		int index = random.nextInt(operadores.size());
+		return (Operador) operadores.toArray()[index];
+	}
+
+	private char simbolo() {
+		int index;
+
+		if (simbolos.size() > 0) {
+			index = random.nextInt(simbolos.size());
+			char simbolo = simbolos.get(index);
+			simbolos.remove(index);
+			return simbolo;
+		} else {
+			index = random.nextInt(simbolosRepetidos.size());
+			return simbolosRepetidos.get(index);
+		}
+	}
+
+	private char simboloNoVacio() {
+		int index;
+
+		if (simbolosRepetidos.contains('E')) {
+			List<Character> simbolosParcial;
+			if (simbolos.size() > 1) {
+				simbolosParcial = new ArrayList<>(simbolos);
+				if (simbolosParcial.contains('E'))
+					simbolosParcial.remove(simbolosParcial.indexOf('E'));
+				index = random.nextInt(simbolosParcial.size());
+				char simbolo = simbolosParcial.get(index);
+				simbolos.remove(index);
+				return simbolo;
+			} else {
+				simbolosParcial = new ArrayList<>(simbolosRepetidos);
+				simbolosParcial.remove(simbolosParcial.indexOf('E'));
+				index = random.nextInt(simbolosParcial.size());
+				return simbolosParcial.get(index);
+			}
+		} else
+			return simbolo();
 	}
 }
--- src/test/java/es/ubu/inf/tfg/regex/datos/GeneradorTest.java
+++ src/test/java/es/ubu/inf/tfg/regex/datos/GeneradorTest.java
@@ -1,65 +1,26 @@
 package es.ubu.inf.tfg.regex.datos;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 public class GeneradorTest {
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	/**
-	 * Comprueba que el generador toma las propiedades correctas en cuanto al
-	 * uso de vacíos.
-	 */
-	@Test
-	public void testUsaVacio() {
-		Generador generador = new Generador(0, true);
-		assertTrue("Generador cree no usar vacíos incorrectamente.",
-				generador.usaVacio());
-
-		generador = new Generador(0, false);
-		assertFalse("Generador cree usar vacíos incorrectamente.",
-				generador.usaVacio());
-	}
-
-	/**
-	 * Comprueba que el generador toma las propiedades correctas en cuanto al
-	 * número de símbolos.
-	 */
-	@Test
-	public void testSimbolos() {
-		Generador generador = new Generador(5, true);
-		assertEquals(
-				"Generador con vacíos contiene número erroneo de símbolos.", 6,
-				generador.simbolos());
-
-		generador = new Generador(5, false);
-		assertEquals(
-				"Generador sin vacíos contiene número erroneo de símbolos.", 5,
-				generador.simbolos());
-	}
 
 	/**
 	 * Comprueba que el generador produce árboles de la profundidad correcta,
 	 * utilizando símbolos correctos.
 	 */
 	@Test
-	public void testSubArbol() {
-		Generador generador = new Generador(2, true);
-		ExpresionRegular expresion = generador.subArbol(8, null);
+	public void testArbol() {
+		Generador generador = new Generador();
+		ExpresionRegular expresion = generador.arbol(8, 2, true);
 
 		assertEquals("Generado árbol de profundidad erronea", 8,
+				profundidad(expresion, 0));
+
+		expresion = generador.arbol(12, 5, false);
+
+		assertEquals("Generado árbol de profundidad erronea", 12,
 				profundidad(expresion, 0));
 	}
 
