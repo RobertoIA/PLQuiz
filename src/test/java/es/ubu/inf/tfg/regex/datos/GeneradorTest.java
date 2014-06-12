805775adfef1051e17ee4b2d2e76e21d44951237
Roberto Izquierdo <RobertoIA1987@gmail.com>
2014-06-12 20:02:01 +0200

    Añade test para investigar expresiones.

--- src/test/java/es/ubu/inf/tfg/regex/datos/GeneradorTest.java
+++ src/test/java/es/ubu/inf/tfg/regex/datos/GeneradorTest.java
@@ -1,10 +1,24 @@
 package es.ubu.inf.tfg.regex.datos;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import es.ubu.inf.tfg.regex.asu.AhoSethiUllman;
+import es.ubu.inf.tfg.regex.thompson.ConstruccionSubconjuntos;
 
 public class GeneradorTest {
+
+	private static final Logger log = LoggerFactory
+			.getLogger(GeneradorTest.class);
+
+	private final int ITERACIONES = 100_000;
+	private final int MAX_PROFUNDIDAD = 6;
 
 	/**
 	 * Comprueba que el generador produce árboles de la profundidad correcta,
@@ -51,5 +65,106 @@ public class GeneradorTest {
 				expresion.equals(mutante));
 		assertTrue("La expresión mutada tiene una profundidad erronea.",
 				Math.abs(8 - mutante.profundidad()) <= 1);
+	}
+
+	/**
+	 * Genera una serie de problemas Aho-Sethi-Ullman a partir de expresiones
+	 * regulares aleatorias y almacena sus caracteristicas.
+	 * <p>
+	 * No incluye nodos vacíos.
+	 */
+	@Test
+	@Ignore
+	public void testGeneracionASUNoVacio() {
+		genera(ITERACIONES, false, "ASU", MAX_PROFUNDIDAD);
+	}
+
+	/**
+	 * Genera una serie de problemas de Construcción de Subconjuntos a partir de
+	 * expresiones regulares aleatorias y almacena sus caracteristicas.
+	 * <p>
+	 * No incluye nodos vacíos.
+	 */
+	@Test
+	@Ignore
+	public void testGeneracionCSNoVacio() {
+		genera(ITERACIONES, false, "CS", MAX_PROFUNDIDAD);
+	}
+
+	/**
+	 * Genera una serie de problemas Aho-Sethi-Ullman a partir de expresiones
+	 * regulares aleatorias y almacena sus caracteristicas.
+	 * <p>
+	 * Incluye nodos vacíos.
+	 */
+	@Test
+	@Ignore
+	public void testGeneracionASUVacio() {
+		genera(ITERACIONES, true, "ASU", MAX_PROFUNDIDAD);
+	}
+
+	/**
+	 * Genera una serie de problemas de Construcción de Subconjuntos a partir de
+	 * expresiones regulares aleatorias y almacena sus caracteristicas.
+	 * <p>
+	 * Incluye nodos vacíos.
+	 */
+	@Test
+	@Ignore
+	public void testGeneracionCSVacio() {
+		genera(ITERACIONES, true, "CS", MAX_PROFUNDIDAD);
+	}
+
+	/**
+	 * Genera una serie de instancias de expresiones regulares, con o sin nodos
+	 * vacíos, y analiza sus características de acuerdo con un tipo de problema
+	 * dado.
+	 * <p>
+	 * Los resultados se almacenan en el log.
+	 */
+	private void genera(int n, boolean vacio, String tipo, int profundidad) {
+		Generador generador;
+		ExpresionRegular expresion;
+		int it;
+
+		switch (tipo) {
+		case "ASU":
+			AhoSethiUllman asu;
+			generador = new Generador(6, vacio, true);
+
+			while (profundidad > 0) {
+				it = n;
+				log.info("profundidad {}", profundidad);
+				while (it > 0) {
+					expresion = generador.arbol(profundidad);
+					asu = new AhoSethiUllman(expresion);
+
+					log.info("{} {}", asu.simbolos().size(), asu.estados()
+							.size());
+
+					it--;
+				}
+				profundidad--;
+			}
+			break;
+		case "CS":
+			ConstruccionSubconjuntos cs;
+			generador = new Generador(6, vacio, false);
+
+			while (profundidad > 0) {
+				it = n;
+				log.info("profundidad {}", profundidad);
+				while (it > 0) {
+					expresion = generador.arbol(profundidad);
+					cs = new ConstruccionSubconjuntos(expresion);
+
+					log.info("{} {}", cs.simbolos().size(), cs.estados().size());
+
+					it--;
+				}
+				profundidad--;
+			}
+			break;
+		}
 	}
 }
